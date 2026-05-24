c2ac82504dc7bfb3af19d940ac090e10ae3ea926
Stefan Drissen <stefan.drissen@gmail.com>
2026-05-24 14:56:59 +0200

    implement logic test has(iA)

--- src/logic/test/has.s
+++ src/logic/test/has.s
@@ -1,3 +1,19 @@
+; has(itm iA)
+
+; The has command returns TRUE if the specified inventory item is in the player's inventory (i.e. the item's room number equals 255).
 logic.test.has:
 
-    jp logic.test.invalid
+    ld h,object.locations.high
+    ld l,(ix+0)
+    inc ix
+
+    ld a,(hl)
+    cp 0xff
+    jr z,@true
+
+    xor a   ; false
+    ret
+
+@true:
+    inc a
+    ret
